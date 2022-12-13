abff70e949bc368dcf8186f8c8c32938240df4a1
Quentin <quentin.geerts@bstorm.be>
2022-12-13 13:41:46 +0100

    xhr - xml

--- Demo/XHR/xml/script.js
+++ Demo/XHR/xml/script.js
@@ -9,9 +9,11 @@ function initXHR () {
 
         if (this.readyState === xhr.DONE && this.status == 200) {
 
-            const docXML = xhr.responseXML
+            const docXML = xhr.responseXML;
 
-            getAllAnimes(docXML);
+            const animeList = getAllAnimes(docXML);
+
+            generateRow(animeList);
         }
 
     };
@@ -20,18 +22,45 @@ function initXHR () {
     xhr.send();
 }
 
-function getAllAnimes(xml) {
+function getAllAnimes (xml) {
 
     // Récupération de la liste des balises "anime"
     const animes = xml.getElementsByTagName('anime');
+    const animeList = [];
 
     for (const anime of animes) {
-        
+
         // console.log(anime);
         // const title = anime.getElementsByTagName('series_title')[0].textContent
-        const title = anime.querySelector('series_title').textContent
-        console.dir(title);
+        const title = anime.querySelector('series_title').textContent;
+        const type = anime.querySelector('series_type').textContent;
+        const episodesMax = anime.querySelector('series_episodes').textContent;
 
+        animeList.push({ title, type, episodesMax });
     }
 
+    return animeList;
+}
+
+function generateRow (animes) {
+    const content = document.getElementById('content');
+
+    for (const anime of animes) {
+        // Création des éléments du tableau
+        const animeRow = document.createElement('tr');
+        const animeTitle = document.createElement('td')
+        const animeType = document.createElement('td')
+        const animeEpisodes = document.createElement('td')
+
+        // Paramétrer les éléments HTML
+        animeTitle.innerText = anime.title;
+        animeType.innerText = anime.type;
+        animeEpisodes.innerText = anime.episodesMax;
+
+        // Ajouter les enfants aux parents
+        animeRow.appendChild(animeTitle)
+        animeRow.appendChild(animeType)
+        animeRow.appendChild(animeEpisodes)
+        content.appendChild(animeRow)
+    }
 }
